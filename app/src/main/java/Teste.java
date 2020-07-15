6185e1cbff13450e2bb6cbbee1e167e0cdc70e33
Eric Alencar <eric_tonarede@hotmail.com>
2020-07-15 20:39:42 -0300

    envio dos arquivos do projeto de exemplo

---
+++ app/src/main/java/Teste.java
@@ -0,0 +1,12 @@
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Teste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("It's a me Mario!");
+    }
+}
